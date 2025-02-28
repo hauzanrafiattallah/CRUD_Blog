3d5f10777736d10387dae84ab6a7541c36e47b6a
Hauzan Rafi Attallah <rafihauzan42@gmail.com>
2025-02-28 23:04:46 +0700

    EX-5 | feat: API Update Post Blog

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -12,10 +12,10 @@ const getPosts = async (req, res) => {
     });
   } catch (error) {
     console.log(error);
-
     return res.status(500).json({
       success: false,
       message: "Get data posts failed",
+      data: null,
     });
   }
 };
@@ -54,6 +54,50 @@ const createPost = async (req, res) => {
     return res.status(500).json({
       success: false,
       message: "Create post failed",
+      data: null,
+    });
+  }
+};
+
+const updatePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const parse = postSchema.safeParse(req.body);
+
+    if (!parse.success) {
+      const errorMessages = parse.error.issues.map(
+        (error) => `${error.path} - ${error.message}`
+      );
+      return res.status(400).json({
+        success: false,
+        message: errorMessages,
+        data: null,
+      });
+    }
+
+    const post = await prisma.post.update({
+      where: {
+        id: parseInt(id),
+      },
+      data: {
+        title: parse.data.title,
+        author_name: parse.data.author_name,
+        content: parse.data.content,
+        published: parse.data.published,
+      },
+    });
+    return res.json({
+      success: true,
+      message: "Update post successfully",
+      data: post,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Update post failed",
+      data: null,
     });
   }
 };
@@ -61,4 +105,5 @@ const createPost = async (req, res) => {
 module.exports = {
   getPosts,
   createPost,
+  updatePost,
 };
--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { getPosts, createPost } = require("../controllers/postController");
+const {
+  getPosts,
+  createPost,
+  updatePost,
+} = require("../controllers/postController");
 
 const postRoutes = express.Router();
 
 postRoutes.get("/posts", getPosts);
 postRoutes.post("/posts", createPost);
+postRoutes.put("/posts/:id", updatePost);
 
 module.exports = postRoutes;
